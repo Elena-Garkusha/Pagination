1e12882f4ff4b762566e581e5c019e0a9791fd16
Elena Garkusha <st.elenka.1206@gmail.com>
2016-07-06 11:22:43 +0300

    table with css and js

--- controllers.py
+++ controllers.py
@@ -8,3 +8,21 @@ class Pagination(http.Controller):
          return http.request.render('pagination.index', {
              'products': Products.search([])
          })
+    # @http.route('/pagination/page2/', auth='public', website=True)
+    # def page2(self, **kw):
+    #      Products2 = http.request.env['pagination.pagination']
+    #      return http.request.render('pagination.page2', {
+    #          'products2': Products2.search([])
+    #      })
+    # @http.route('/pagination/page3/', auth='public', website=True)
+    # def page3(self, **kw):
+    #      Products = http.request.env['pagination.pagination']
+    #      return http.request.render('pagination.page3', {
+    #          'products': Products.search([])
+    #      })
+    # @http.route('/pagination/page4/', auth='public', website=True)
+    # def page4(self, **kw):
+    #      Products = http.request.env['pagination.pagination']
+    #      return http.request.render('pagination.page4', {
+    #          'products': Products.search([])
+    #      })
